9efd64de4dad23031c3266c08b1ba5f1b8423f1b
Gulabi-Dil <thegreatmindhehe@gmail.com>
2026-03-13 02:45:49 +0530

    fix mobile layout again

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import Post from './pages/post'
-
+import ScrollToTop from './components/ScrollToTop'
 function App() {
-
+  
   return (
     <>
       <main>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/post/:slug" element={<Post />} />
